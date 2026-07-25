5540b3cf2e1dd99d437cd9a04c6c7929c70964d4
Hugo <hugo.talibart@gmail.com>
2026-07-25 14:34:18 +0200

    style: reorganize import blocks

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,17 @@
-from typing import Any
-from collections.abc import Iterator, Iterable
-
 import os
-from pathlib import Path
-import duckdb
-from datetime import date, timedelta
 import random
 import secrets
+from collections.abc import Iterable, Iterator
+from datetime import date, timedelta
+from pathlib import Path
+from typing import Any
 
-from fastapi import FastAPI, HTTPException, Query, APIRouter, Request, Depends, Body, Header
+import duckdb
+from fastapi import APIRouter, Body, Depends, FastAPI, Header, HTTPException, Query, Request
 from fastapi.middleware.cors import CORSMiddleware
-
 from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
 
 LANGUAGES = {"en", "fr"}
 MIN_NB_LINKS_FOR_TARGET = 20
