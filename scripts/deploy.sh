0316bc10559ea390cb73ddda62f70c3d6dd2ad15
Hugo <hugo.talibart@gmail.com>
2026-07-26 20:01:47 +0200

    chore: check that production API is responding after deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -100,3 +100,6 @@ if [ "$CADDY_CHANGED" = true ]; then
 fi
 
 echo "Deployment successful"
+
+curl --fail --silent http://127.0.0.1:8000/openapi.json > /dev/null
+echo "Production API is responding"
