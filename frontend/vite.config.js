4aab44ca932315f9967c896ce3feddbeb34013e9
Hugo <hugo.talibart@gmail.com>
2026-07-26 19:24:17 +0200

    chore: make it possible to run with different API target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
+import process from "node:process";
+
+const apiTarget = process.env.VITE_API_TARGET ?? "http://127.0.0.1:8000";
+
 export default {
   server: {
     proxy: {
-      "/api": "http://127.0.0.1:8000",
+      "/api": apiTarget,
     },
   },
 };
